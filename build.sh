ce2f507ff88db07d9933ea9313ab4f3e350c01b6
mo <mo@gladmo.com>
2026-03-27 10:18:13 +0800

    embed wasm size to index.html

--- build.sh
+++ build.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 cargo build --profile wasm-release --target wasm32-unknown-unknown
+
 wasm-bindgen \
             --out-dir ./out \
             --target web \
             --no-typescript \
             target/wasm32-unknown-unknown/wasm-release/bevy-hehe.wasm
+
 cp index.html ./out/
-          cp -r assets ./out/
+cp -r assets ./out/
+
+WASM_SIZE=$(wc -c < ./out/bevy-hehe_bg.wasm | tr -d ' ')
+sed "s/{{WASM_SIZE}}/${WASM_SIZE}/" ./out/index.html > ./out/index.html.tmp
+mv ./out/index.html.tmp ./out/index.html
